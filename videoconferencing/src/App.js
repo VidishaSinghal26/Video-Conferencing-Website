f81c8ac944b1c8ce951db8e4368a033e34e57f80
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2023-07-06 10:50:41 +0530

    updated

--- videoconferencing/src/App.js
+++ videoconferencing/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function App() {
+export default function App() {
+  const roomID = "vidisha";
+  let myMeeting = async (element) => {
+
+ // generate Kit Token
+ const appID = 1749655600;
+ const serverSecret = "4e9c7fe260533b4247794961e7b4e514";
+ const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  Date.now().toString(),  "Vidisha");
+
+ // Create instance object from Kit Token.
+ const zp = ZegoUIKitPrebuilt.create(kitToken);
+ // start the call
+ zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Personal link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+         mode: ZegoUIKitPrebuilt.VideoConference,
+        },
+   });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
   );
 }
-
-export default App;
